e674c1193c0ab65b6145ecc319208d0404330ad5
Sudhanshu <rishuparashar7@gmail.com>
2022-07-21 21:39:54 +0530

    first comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState, useEffect} from 'react';
 import './App.css';
+import { db } from './firebase-config';
+import { collection, doc, getDocs } from 'firebase/firestore';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      getUsers(data.data.map((doc) => ({...doc.data(), id: doc.id})));
+    };
+
+    getUsers()
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    {users.map((user) => {
+        return (
+          <div>
+            {" "}
+            <h1>Name: {user.name}</h1>
+            <h1>Class: {user.class}</h1>
+          </div>
+        );
+    })}
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from '@firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7zJlhxKJCH3tODV5YDoq3j8adgjSumbo",
+  authDomain: "internship-a1312.firebaseapp.com",
+  projectId: "internship-a1312",
+  storageBucket: "internship-a1312.appspot.com",
+  messagingSenderId: "1009020917714",
+  appId: "1:1009020917714:web:b51b998946ee8738659a0b",
+  measurementId: "G-FCS8E01WH0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
