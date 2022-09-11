f6160ddd2a7d460f31909ce8c035bb60e101a340
Sudhanshu <rishuparashar7@gmail.com>
2022-09-11 17:40:08 +0530

    redeem showlist

--- src/RedeemReq.js
+++ src/RedeemReq.js
@@ -1,9 +1,46 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import {
+    getAllReddemRequests
+  } from "./FirestoreFun"
+import "./style.css";
 
 function RedeemReq () {
     const [loadingRedeem, setLoadingRedeem] = useState(0);
-    let dataToShow = [];
+    const [dataToShow, setDataToShow] = useState([]);
+    const [selectedItemsRedeem, setSelectedItemsRedeem] = useState([]);
 
+    const getData = async() =>{
+        let data = await getAllReddemRequests();
+        setDataToShow(data);
+    }
+
+    useEffect(() => {
+        if (dataToShow.length < 1) {
+            getData();
+        }
+    }, [ 1 ]);
+
+    const handleCbClick =(e, mail) =>{
+        const checked = e.target.checked;
+        const email = mail;
+        if(email === "Select All" ){
+            let tempData = dataToShow.map((data)=>{
+                return { ...data, isChecked:checked}
+            })
+            setDataToShow(tempData);
+        }else{
+            let tempData = dataToShow.map((data)=>
+                data.email === email ? { ...data, isChecked: checked} : data
+            );
+            setDataToShow(tempData)
+        }
+        
+    }
+
+    const click= () =>{
+        
+        console.log(Math.floor(rand));
+    }
   return (
     <>
     {
@@ -15,21 +52,51 @@ function RedeemReq () {
                     </div>
                 </div>
                 <div className="top-right">
-                    <button className="btn" onClick={rejectSelected}>Done Selected</button>
-                    <button className="btn" onClick={approveSelected}>Reject selected</button>
+                    <button className="btn" onClick={click}>Done Selected</button>
+                    <button className="btn" >Remove selected</button>
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <div className="div2">
+                        <nav>
+                            <h1 style={{ color: 'white' }}>Records</h1>
+                            <div style={{display:"flex", justifyContent:"center"}}>
+                                <label style={{paddingRight:"10px", color:"white"}}>Select All</label>
+                                <input type="checkbox" onChange={(event)=>{handleCbClick(event, "Select All")}} checked={dataToShow.filter((data) => data?.isChecked !== true).length <1
+                                }/>
+                            </div>
+                            <ul>
+                                {
+                                    dataToShow.map((value,key) =>{
+                                        return(
+                                            <div key={key} style={{ marginBottom: "24px", color: 'white', display: 'flex' }}>
+                                            <ul style={{ width: "80%" }}>
+                                                <li>{value.email}</li>
+                                                <li>{`Date: ${value.dateTime.split(",")[0]}`}</li>
+                                                <li>{`Amount: ₹${value.amount}`}</li>
+                                                <li>{`${value.method}: ${value.methodDetail}`}</li>
+                                            </ul>
+                                            <ul style={{display:"flex", justifyContent:"center"}}>
+                                                <input name={value.email} type="checkbox" style={{marginRight:"20px"}} checked={value?.isChecked || false } onChange={(event)=>{handleCbClick(event, value.email)}}/>
+                                            </ul>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
             </div>
         ) : (
-            <div>
-                <nav>
-                    <h1 style={{ color: 'white' }}>Records</h1>
-                    <ul>
-                        {
-
-                        }
-                    </ul>
-                </nav>
+            <div style={{display:"flex" , alignItems:"center" , justifyContent : "center"}}>
+          <div className="loadingio-spinner-spinner-d5vda8qm7j5"><div className="ldio-9u8gbddmqad">
+            <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
             </div>
+          </div>
+          <div>
+            <h1>Loading...</h1>
+          </div>
+          </div>
         )
     }
     </>
